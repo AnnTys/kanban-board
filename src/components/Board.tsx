86dbfc0ec0ae56ebfed7a0820d6b2620499c0dd8
Anna <tyshchenko.ganna@gmail.com>
2025-03-13 21:16:17 +0200

    finish in UI

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,40 +1,43 @@
-import { Droppable, Draggable } from "@hello-pangea/dnd";
-import React from "react";
-import Item, { ItemType } from "./Item";
+import { Droppable, Draggable } from '@hello-pangea/dnd';
+import React from 'react';
+import Item, { ItemType } from './Item';
+import { Container } from 'react-bootstrap';
 
 interface BoardProps {
-  columnId: string;
-  items: ItemType[];
+	columnId: string;
+	items: ItemType[];
 }
 
 const Board: React.FC<BoardProps> = ({ columnId, items }) => {
-  return (
-    <Droppable droppableId={columnId} >
-      {(provided) => (
-        <div 
-          ref={provided.innerRef} 
-          {...provided.droppableProps} 
-          className="p-3 min-h-40"
-        >
-          {items.map((item, index) => (
-            <Draggable key={item.id} draggableId={item.id} index={index}>
-              {(provided) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                  className="mb-2"
-                >
-                  <Item item={item} />
-                </div>
-              )}
-            </Draggable>
-          ))}
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
-  );
+	return (
+		<Container className="border border-secondary bg-secondary p-3">
+			<Droppable droppableId={columnId}>
+				{(provided) => (
+					<div
+						ref={provided.innerRef}
+						{...provided.droppableProps}
+						className="p-3 min-h-40"
+					>
+						{items.map((item, index) => (
+							<Draggable key={item.id} draggableId={item.id} index={index}>
+								{(provided) => (
+									<div
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										className="mb-2"
+									>
+										<Item item={item} />
+									</div>
+								)}
+							</Draggable>
+						))}
+						{provided.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</Container>
+	);
 };
 
 export default Board;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,74 +4,71 @@ import Board from './Board';
 import { useAppDispatch, useColumnsSelector } from '../store/hooks';
 import { updateColumns, saveCookieImpl } from '../store/actions';
 
+const style = "border-0 shadow-none";
 
 export default function Main() {
+	const dispatch = useAppDispatch();
+	const columns = useColumnsSelector((state) => state.columns.columns);
 
-  const dispatch = useAppDispatch();
-  const columns = useColumnsSelector((state) => state.columns.columns);
+	const handleDragEnd = (result: DropResult) => {
+		if (!result.destination) return;
 
-  const handleDragEnd = (result: DropResult) => {
-    if (!result.destination) return;
+		const sourceColumn = result.source.droppableId;
+		const destinationColumn = result.destination.droppableId;
 
-    const sourceColumn = result.source.droppableId;
-    const destinationColumn = result.destination.droppableId;
+		if (sourceColumn === destinationColumn) {
+			const newItems = [...columns[sourceColumn]];
+			const [movedItem] = newItems.splice(result.source.index, 1);
+			newItems.splice(result.destination.index, 0, movedItem);
 
-    if (sourceColumn === destinationColumn) {
-    
-      const newItems = [...columns[sourceColumn]];
-      const [movedItem] = newItems.splice(result.source.index, 1);
-      newItems.splice(result.destination.index, 0, movedItem);
+			const newColumns = {
+				...columns,
+				[sourceColumn]: newItems,
+			};
 
-      const newColumns = {
-        ...columns,
-        [sourceColumn]: newItems,
-      };
-      
-      dispatch(updateColumns(newColumns));
-      dispatch(saveCookieImpl("columns", JSON.stringify(newColumns), 30));
-    } else {
-  
-      const sourceItems = [...columns[sourceColumn]];
-      const destinationItems = [...columns[destinationColumn]];
-      const [movedItem] = sourceItems.splice(result.source.index, 1);
-      destinationItems.splice(result.destination.index, 0, movedItem);
+			dispatch(updateColumns(newColumns));
+			dispatch(saveCookieImpl('columns', JSON.stringify(newColumns), 30));
+		} else {
+			const sourceItems = [...columns[sourceColumn]];
+			const destinationItems = [...columns[destinationColumn]];
+			const [movedItem] = sourceItems.splice(result.source.index, 1);
+			destinationItems.splice(result.destination.index, 0, movedItem);
 
-      const newColumns = {
-        ...columns,
-        [sourceColumn]: sourceItems,
-        [destinationColumn]: destinationItems,
-      };
-      
-      dispatch(updateColumns(newColumns));
-      dispatch(saveCookieImpl("columns", JSON.stringify(newColumns), 30));
-    }
-  };
+			const newColumns = {
+				...columns,
+				[sourceColumn]: sourceItems,
+				[destinationColumn]: destinationItems,
+			};
 
-  return (
-    
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Container className='mainBoard'>
-        <Row className="gy-3 title">
-          <Col xs={12} md={4}>
-            <Card>
-              <Card.Title className="p-3">To Do</Card.Title>
-              <Board columnId="todo" items={columns.todo} />
-            </Card>
-          </Col>
-          <Col xs={12} md={4}>
-            <Card>
-              <Card.Title className="p-3">In Progress</Card.Title>
-              <Board columnId="inProgress" items={columns.inProgress} />
-            </Card>
-          </Col>
-          <Col xs={12} md={4}>
-            <Card>
-              <Card.Title className="p-3">Done</Card.Title>
-              <Board columnId="done" items={columns.done} />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </DragDropContext>
-  );
+			dispatch(updateColumns(newColumns));
+			dispatch(saveCookieImpl('columns', JSON.stringify(newColumns), 30));
+		}
+	};
+
+	return (
+		<DragDropContext onDragEnd={handleDragEnd}>
+			<Container className="mainBoard">
+				<Row className="gy-3 title">
+					<Col xs={12} md={4}>
+						<Card className={style}>
+							<Card.Title className="p-3">To Do</Card.Title>
+							<Board columnId="todo" items={columns.todo} />
+						</Card>
+					</Col>
+					<Col xs={12} md={4}>
+						<Card className={style}>
+							<Card.Title className="p-3">In Progress</Card.Title>
+							<Board columnId="inProgress" items={columns.inProgress} />
+						</Card>
+					</Col>
+					<Col xs={12} md={4}>
+						<Card className={style}>
+							<Card.Title className="p-3">Done</Card.Title>
+							<Board columnId="done" items={columns.done} />
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</DragDropContext>
+	);
 }
